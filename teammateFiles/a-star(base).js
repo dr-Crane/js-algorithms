115d518964f83195d5e1c0eeadf199a908e6adf3
A_Sun <128594008+AsmrS4@users.noreply.github.com>
2023-04-02 02:03:00 +0700

    небольшие правки(почти закончил)

--- teammateFiles/a-star(base).js
+++ teammateFiles/a-star(base).js
@@ -1,13 +1,22 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-const CELL_SIZE = 60;
-var startBttn = document.getElementById("startButton");
-var clearBttn = document.getElementById("clearButton");
 
-var width = 20;
-canvas.width = width * CELL_SIZE;
-canvas.height = width * CELL_SIZE;
-var WALLS=[];
+var CREATE_BUTTON = document.getElementById("createButton");
+var BEGIN_BUTTON = document.getElementById("beginButton");
+var TARGET_BUTTON = document.getElementById("targetButton");
+var WALL_BUTTON = document.getElementById("wallButton");
+var START_BUTTON = document.getElementById("startButton");
+var CLEAR_BUTTON = document.getElementById("clearButton");
+var INPUT_BUTTON = document.getElementById("inputButton");
+
+var width = 30;
+var field;
+
+const CELL_SIZE = canvas.width / width;
+
+var WALLS = [];
+var BEGGIN = [];
+var TARGET = [];
 
 class Cell {
     constructor(x, y, wall = false) {
@@ -28,27 +37,6 @@ class Cell {
     }
 }
 
-function createField(width) { //создаем матрицу в которой каждая ячейка - клетка
-    let field = new Array(width);
-    for (let i = 0; i < width; i++) {
-        field[i] = new Array(width);
-        for (let j = 0; j < width; j++) {
-            field[i][j] = new Cell(i, j);
-        }
-    }
-    return field;
-}
-//добавляем стены на нашей карте
-function addObstacles(field, walls) {
-    for (let i = 0; i < walls.length; i++) {
-        let [row, col] = walls[i];
-        field[row][col].isWall = true;
-    }
-}
-function addWall(wall){
-    drawCell(wall);
-    return WALLS.push(wall);
-}
 function AStarAlgorithm(startCell, targetCell, field) {
     let openList = [startCell];
     let closedList = [];
@@ -111,24 +99,92 @@ function AStarAlgorithm(startCell, targetCell, field) {
     return [];
 }
 
+function createField(width) { //создаем матрицу в которой каждая ячейка - клетка
+    let field = new Array(width);
+    for (let i = 0; i < width; i++) {
+        field[i] = new Array(width);
+        for (let j = 0; j < width; j++) {
+            field[i][j] = new Cell(i, j);
+        }
+    }
+    return field;
+}
+
+function addStart(cell) {
+    cell.isStart = true;
+    return cell;
+}
+function addTarget(cell) {
+    cell.isTarget = true;
+    return cell;
+}
+
+function addBeggin(cell){
+    if(!cell.isStart){
+        cell.isStart = true;
+        drawCell(cell);
+        BEGGIN.push(cell);
+    }
+    if(BEGGIN.length >= 2){
+        BEGGIN[0].isStart = false;
+        drawCell(BEGGIN[0]);
+        BEGGIN.splice(0,1);
+    }
+}
+function addTarget(cell){
+    if(!cell.isTarget){
+        cell.isTarget = true;
+        drawCell(cell);
+        TARGET.push(cell);
+    }
+    if(TARGET.length >= 2){
+        TARGET[0].isTarget = false;
+        drawCell(TARGET[0]);
+        TARGET.splice(0,1);
+    }
+    
+}
+function addWall(cell) {
+
+    if (!(cell.isStart || cell.isTarget)) {
+        if (!cell.isWall) {
+            cell.isWall = true;
+            drawCell(cell);
+            WALLS.unshift(cell);
+        } else {
+            cell.isWall = false;
+            const index = WALLS.shift();
+            drawCell(cell);
+            WALLS.splice(index, 1)
+        }
+    }
+    return WALLS;
+}
+
 function pathIsExist(path) {
     return path.length != 0;
 }
 
 function drawCell(cell) {
     let color;
-    if (cell.isPath) {
+    if (cell.isStart) {
+        color = '#9ACD32';
+    } else if (cell.isTarget) {
+        color = '#6495ED';
+    }
+    else if (cell.isPath) {
         color = '#FFE4B5';
     }
     else if (cell.isWall) {
         color = '#A9A9A9';
-    } else {
+    }
+    else {
         color = '#FFFFFF';
     }
     ctx.fillStyle = color;
     ctx.fillRect(cell.y * CELL_SIZE, cell.x * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-    ctx.strokeStyle = '#696969';
-    ctx.strokeRect(cell.y * CELL_SIZE, cell.x * CELL_SIZE, CELL_SIZE, CELL_SIZE);
+    // ctx.strokeStyle = '#696969';
+    // ctx.strokeRect(cell.y * CELL_SIZE, cell.x * CELL_SIZE, CELL_SIZE, CELL_SIZE);
 }
 
 function drawPath(path) {
@@ -139,7 +195,7 @@ function drawPath(path) {
     }
 }
 
-function drawMap(field, startCell, targetCell) {
+function drawMap(field) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     for (let i = 0; i < field.length; i++) {
         for (let j = 0; j < field[i].length; j++) {
@@ -147,41 +203,55 @@ function drawMap(field, startCell, targetCell) {
             drawCell(cell);
         }
     }
-    ctx.fillStyle = '#9ACD32';
-    ctx.fillRect(startCell.y * CELL_SIZE, startCell.x * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-    ctx.fillStyle = '#6495ED';
-    ctx.fillRect(targetCell.y * CELL_SIZE, targetCell.x * CELL_SIZE, CELL_SIZE, CELL_SIZE);
-
 }
 
 function getRandomCell(field) {
     let row = Math.floor(Math.random() * field.length);
     let col = Math.floor(Math.random() * field[0].length);
-    return(!field[row][col].isWall)?field[row][col]:getRandomCell(field);
+    return (!field[row][col].isWall) ? field[row][col] : getRandomCell(field);
 }
 
-var field = createField(width);
+//<---------------------------MAIN------------------------------->
 
-canvas.onclick = function (event){
-    let x = event.offsetX;
-    let y = event.offsetY;
-    x=Math.floor(x/CELL_SIZE);
-    y=Math.floor(y/CELL_SIZE);
-    field[y][x].isWall=true;
-    addWall(field[y][x]);
-}
-
-addObstacles(field,WALLS);
-
-let start = getRandomCell(field);
-let end = getRandomCell(field);
-
-drawMap(field, start, end);
-
-startBttn.addEventListener('click', () => {
-    let path = AStarAlgorithm(start, end, field);
-    return pathIsExist(path)?drawPath(path):alert("path doesn't exist:(")
+CREATE_BUTTON.addEventListener('click',()=>{
+    field = createField(width);
+    drawMap(field);
 })
-clearBttn.addEventListener('click',()=>{
+
+BEGIN_BUTTON.addEventListener('click',()=>{
+
+    canvas.onclick = function (event) {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        x = Math.floor(x / CELL_SIZE);
+        y = Math.floor(y / CELL_SIZE);
+       addBeggin(field[y][x]);
+    }
+
+});
+TARGET_BUTTON.addEventListener('click',()=>{
+
+    canvas.onclick = function (event) {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        x = Math.floor(x / CELL_SIZE);
+        y = Math.floor(y / CELL_SIZE);
+        addTarget(field[y][x]);
+    }
+})
+WALL_BUTTON.addEventListener('click',()=>{
+    canvas.onclick = function (event) {
+        let x = event.offsetX;
+        let y = event.offsetY;
+        x = Math.floor(x / CELL_SIZE);
+        y = Math.floor(y / CELL_SIZE);
+        addWall(field[y][x]);
+    }
+});
+START_BUTTON.addEventListener('click', () => {
+    let path = AStarAlgorithm(BEGGIN[0], TARGET[0], field);
+    return pathIsExist(path) ? drawPath(path) : alert("path doesn't exist:(")
+});
+CLEAR_BUTTON.addEventListener('click', () => {
     window.location.reload();
-})
+});
