608bd7cd7b40a1c027b329c94c70e568f65ab996
drCrane <bekzat.kanatbekulu@gmail.com>
2023-04-22 13:40:57 +0700

    Последние правки

--- src/page/AStarAlgorithm.js
+++ src/page/AStarAlgorithm.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const AStarAlgorithm = () => {
+    useEffect(() => {
+        window.location.href = 'http://127.0.0.1:5500/A_STAR_ALGORITHM/a-star.html';
+    })
+
     return (
         <div>
             <h1 className={'text-light'}>A*</h1>
--- src/page/ClusterAnalysisAlgorithm.js
+++ src/page/ClusterAnalysisAlgorithm.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 const ClusterAnalysisAlgorithm = () => {
+    useEffect(() => {
+        window.location.href = 'http://127.0.0.1:5500/CLUSTER_ALGORITHM/cluster.html';
+    })
+
     return (
         <div>
             <h1 className={'text-light'}>Cluster analysis algorithm</h1>
